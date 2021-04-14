3a64b34d8010e3cf0439edd806a2b86039daf650
Ryan Niemi <40528017+J3T4R0@users.noreply.github.com>
2021-04-13 23:27:45 -0700

    Update FrontController.hs

--- Web/FrontController.hs
+++ Web/FrontController.hs
@@ -6,14 +6,22 @@ import Web.Controller.Dashboard
 import Web.Controller.Prelude
 import Web.View.Layout (defaultLayout)
 
+-- Controller Imports
+import Web.Controller.Admins
+import Web.Controller.Static
+import Web.Controller.Sessions
+import IHP.LoginSupport.Middleware
+
 instance FrontController WebApplication where
   controllers =
     [ startPage DashboardAction,
       parseRoute @DashboardController
       -- Generator Marker
+    , parseRoute @AdminsController
     ]
 
 instance InitControllerContext WebApplication where
   initContext = do
     setLayout defaultLayout
+    initAuthentication @Admin
     initAutoRefresh
