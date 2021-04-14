e418b63944792dfe5d4eb55fe8928293b312a182
Ryan Niemi <40528017+J3T4R0@users.noreply.github.com>
2021-04-14 00:23:18 -0700

    Update Fixtures.sql

--- Application/Fixtures.sql
+++ Application/Fixtures.sql
@@ -42,5 +42,56 @@ ALTER TABLE public.messages DISABLE TRIGGER ALL;
 
 INSERT INTO public.messages (id, room_id, body, created_at, user_name) VALUES ('7b06221a-ee96-44b7-bf48-10cda6caca46', 'a1051d2c-20b3-4703-8c56-b1fc77434860', 'Hello There!', '2021-02-24 14:29:50.790191-05', 'CustomUser');
 
-
 ALTER TABLE public.messages ENABLE TRIGGER ALL;
+
+ALTER TABLE public.clubs DISABLE TRIGGER ALL;
+
+ALTER TABLE public.clubs ENABLE TRIGGER ALL;
+
+ALTER TABLE public.tournois DISABLE TRIGGER ALL;
+
+ALTER TABLE public.tournois ENABLE TRIGGER ALL;
+
+ALTER TABLE public.equipes DISABLE TRIGGER ALL;
+
+ALTER TABLE public.equipes ENABLE TRIGGER ALL;
+
+ALTER TABLE public.gymnases DISABLE TRIGGER ALL;
+
+ALTER TABLE public.gymnases ENABLE TRIGGER ALL;
+
+ALTER TABLE public.matchs DISABLE TRIGGER ALL;
+
+ALTER TABLE public.matchs ENABLE TRIGGER ALL;
+
+ALTER TABLE public.match_sets DISABLE TRIGGER ALL;
+
+ALTER TABLE public.match_sets ENABLE TRIGGER ALL;
+
+ALTER TABLE public.personnes DISABLE TRIGGER ALL;
+
+ALTER TABLE public.personnes ENABLE TRIGGER ALL;
+
+ALTER TABLE public.tournoi_branches DISABLE TRIGGER ALL;
+
+ALTER TABLE public.tournoi_branches ENABLE TRIGGER ALL;
+
+ALTER TABLE public.tournoi_phases DISABLE TRIGGER ALL;
+
+ALTER TABLE public.tournoi_phases ENABLE TRIGGER ALL;
+
+ALTER TABLE public.tournoi_poules DISABLE TRIGGER ALL;
+
+ALTER TABLE public.tournoi_poules ENABLE TRIGGER ALL;
+
+ALTER TABLE public.poule_equipes DISABLE TRIGGER ALL;
+
+ALTER TABLE public.poule_equipes ENABLE TRIGGER ALL;
+
+ALTER TABLE public.poule_matchs DISABLE TRIGGER ALL;
+
+ALTER TABLE public.poule_matchs ENABLE TRIGGER ALL;
+
+ALTER TABLE public.terrains DISABLE TRIGGER ALL;
+
+ALTER TABLE public.terrains ENABLE TRIGGER ALL;
