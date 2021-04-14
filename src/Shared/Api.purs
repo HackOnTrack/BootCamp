b0518b86998a1c2594a1981b799e8c0d938798d3
Ryan Niemi <40528017+J3T4R0@users.noreply.github.com>
2021-04-13 21:54:03 -0700

    Create Api.purs

---
+++ src/Shared/Api.purs
@@ -0,0 +1,9 @@
+module Pure.Api where
+
+import Prelude
+
+type RunningGame = { id :: String
+                   , name :: String
+                   , startedBy :: String
+                   , public :: Boolean
+                   }
