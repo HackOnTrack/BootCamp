1885d3d16ffc2684e7c8a6301e3c248ce546f59a
Ryan Niemi <40528017+J3T4R0@users.noreply.github.com>
2021-04-13 23:03:17 -0700

    Update Routes.hs

--- Web/Routes.hs
+++ Web/Routes.hs
@@ -1,7 +1,9 @@
 module Web.Routes where
-import IHP.RouterPrelude
+
 import Generated.Types
+import IHP.RouterPrelude
 import Web.Types
 
 -- Generator Marker
-instance AutoRoute StaticController
+
+instance AutoRoute DashboardController
