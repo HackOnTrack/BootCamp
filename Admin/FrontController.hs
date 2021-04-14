89155e398f89769dcbf65dad96dcd6da0e41630b
Ryan Niemi <niemiryan@gmail.com>
2021-04-14 00:06:38 -0700

    added controllers

--- Admin/FrontController.hs
+++ Admin/FrontController.hs
@@ -14,16 +14,43 @@ import Admin.Controller.Static
 import IHP.LoginSupport.Middleware
 import Admin.Controller.Sessions
 
+import Admin.Controller.MatchSets
+import Admin.Controller.Matchs
+import Admin.Controller.Terrains
+import Admin.Controller.PouleMatchs
+import Admin.Controller.PouleEquipes
+import Admin.Controller.Gymnases
+import Admin.Controller.TournoiPoules
+import Admin.Controller.TournoiBranches
+import Admin.Controller.TournoiPhases
+import Admin.Controller.Equipes
+import Admin.Controller.Personnes
+import Admin.Controller.Tournois
+import Admin.Controller.Clubs
+import Admin.Controller.Static
+
 instance FrontController AdminApplication where
     controllers = 
         [ startPage WelcomeAction
         , parseRoute @StaticController
         , parseRoute @SessionsController
-        -- Generator Marker
         , parseRoute @PostsController
         , parseRoute @UploadsController
         , parseRoute @UsersController
         , parseRoute @AdminsController
+        , parseRoute @MatchSetsController
+        , parseRoute @MatchsController
+        , parseRoute @TerrainsController
+        , parseRoute @PouleMatchsController
+        , parseRoute @PouleEquipesController
+        , parseRoute @GymnasesController
+        , parseRoute @TournoiPoulesController
+        , parseRoute @TournoiBranchesController
+        , parseRoute @TournoiPhasesController
+        , parseRoute @EquipesController
+        , parseRoute @PersonnesController
+        , parseRoute @TournoisController
+        , parseRoute @ClubsController
         ]
 
 instance InitControllerContext AdminApplication where
--- Admin/Routes.hs
+++ Admin/Routes.hs
@@ -18,3 +18,41 @@ instance AutoRoute UploadsController
 
 instance AutoRoute PostsController
 
+instance AutoRoute StaticController
+
+instance AutoRoute ClubsController
+
+instance AutoRoute TournoisController
+
+
+instance AutoRoute PersonnesController
+
+
+instance AutoRoute EquipesController
+
+
+instance AutoRoute TournoiPhasesController
+
+
+instance AutoRoute TournoiBranchesController
+
+
+instance AutoRoute TournoiPoulesController
+
+
+instance AutoRoute GymnasesController
+
+
+instance AutoRoute PouleEquipesController
+
+
+instance AutoRoute PouleMatchsController
+
+
+instance AutoRoute TerrainsController
+
+
+instance AutoRoute MatchsController
+
+
+instance AutoRoute MatchSetsController
