ff2339b13b834a7d1a93d2198b90ce8939ce9879
Ryan Niemi <40528017+J3T4R0@users.noreply.github.com>
2021-04-13 23:24:52 -0700

    Update Types.hs

--- Web/Types.hs
+++ Web/Types.hs
@@ -3,6 +3,7 @@ module Web.Types where
 import Generated.Types
 import IHP.ModelSupport
 import IHP.Prelude
+import IHP.LoginSupport.Types
 
 data WebApplication
   = WebApplication
@@ -16,3 +17,28 @@ data DashboardController
   | CreateRoomDashboardAction
   | CreateMessageDashboardAction {roomId :: Id Room}
   deriving (Eq, Show, Data)
+
+data SessionsController
+    = NewSessionAction
+    | CreateSessionAction
+    | DeleteSessionAction
+    deriving (Eq, Show, Data)
+
+instance HasNewSessionUrl User where
+    newSessionUrl _ = "/"
+
+instance HasNewSessionUrl Admin where
+    newSessionUrl _ = "/"
+
+type instance CurrentAdminRecord = Admin
+type instance CurrentUserRecord = User
+
+data AdminsController
+    = AdminsAction
+    | NewAdminAction
+    | ShowAdminAction { adminId :: !(Id Admin) }
+    | CreateAdminAction
+    | EditAdminAction { adminId :: !(Id Admin) }
+    | UpdateAdminAction { adminId :: !(Id Admin) }
+    | DeleteAdminAction { adminId :: !(Id Admin) }
+    deriving (Eq, Show, Data)
